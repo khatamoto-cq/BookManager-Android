d93326685cd3b700907bb943cb0c376326bbf508
hatamoto kanji <k.hatamoto@caraquri.com>
2017-06-20 16:13:23 +0900

    Splashは通信がないのでscheduleの依存を外した

--- app/src/main/java/com/caraquri/hatamoto/bookmanager/presentation/SplashPresenter.java
+++ app/src/main/java/com/caraquri/hatamoto/bookmanager/presentation/SplashPresenter.java
@@ -3,15 +3,7 @@ package com.caraquri.hatamoto.bookmanager.presentation;
 import com.caraquri.hatamoto.bookmanager.presentation.contract.SplashContract;
 import com.caraquri.hatamoto.bookmanager.util.mvp.BasePresenter;
 
-import io.reactivex.Scheduler;
-
 public class SplashPresenter extends BasePresenter<SplashContract.View> implements SplashContract.Action {
-
-    private Scheduler scheduler;
-
-    public SplashPresenter(Scheduler scheduler) {
-        this.scheduler = scheduler;
-    }
 
     @Override
     public void moveNextScreen() {
