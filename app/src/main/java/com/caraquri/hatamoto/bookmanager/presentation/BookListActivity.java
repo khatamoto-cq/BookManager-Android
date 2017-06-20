5e4afc97819b4a5650aacd4ba67fb23b536e67e1
hatamoto kanji <k.hatamoto@caraquri.com>
2017-06-20 16:27:27 +0900

    各画面のActivityを作成

---
+++ app/src/main/java/com/caraquri/hatamoto/bookmanager/presentation/BookListActivity.java
@@ -0,0 +1,15 @@
+package com.caraquri.hatamoto.bookmanager.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.caraquri.hatamoto.bookmanager.R;
+
+public class BookListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+    }
+}
