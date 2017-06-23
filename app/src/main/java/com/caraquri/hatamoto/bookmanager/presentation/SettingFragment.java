341e72f2459b50f2c2a5b513868d645b418d0bde
hatamoto kanji <k.hatamoto@caraquri.com>
2017-06-23 19:40:08 +0900

    Fragmentの不要なオーバーライドを削除

--- app/src/main/java/com/caraquri/hatamoto/bookmanager/presentation/SettingFragment.java
+++ app/src/main/java/com/caraquri/hatamoto/bookmanager/presentation/SettingFragment.java
@@ -23,11 +23,6 @@ public class SettingFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
